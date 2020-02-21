084468d967009d36ff7ea321ec35b51675ab6442
Brett Bedevian <brettbedevian@gmail.com>
2020-02-21 17:17:00 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -45,7 +45,7 @@ cart_item_w_coupon = find_item_by_name_in_collection(couponed_item, cart)
       cart_item_w_coupon[:count] += coupons[i][:num]
       cart_item[:count] -= coupons[i][:num]
     else 
-      cart_item_w_coupon {
+      cart_item_w_coupon = {
         :item => couponed_item,
         :price => coupons[i][:cost]/coupons[i][:num],
         :count => coupons[i][:num],
