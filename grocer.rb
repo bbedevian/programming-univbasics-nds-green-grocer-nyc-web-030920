eb72d3e90d9ce571eaaaa38218d9a508b656fd3f
Brett Bedevian <brettbedevian@gmail.com>
2020-02-21 15:23:28 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -2,7 +2,7 @@ def find_item_by_name_in_collection(name, collection)
   i = 0 
   while i < collection.length do 
   if name == collection[i][:item] 
-   collection[i]
+   return collection[i]
   else 
    nil 
   end 
